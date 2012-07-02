52c3f233ae2a25427041718595482c11f8eea8d1
Ignacio Nicovani <inicovani@HQ>
2012-07-02 18:10:24 -0400

    Commit inicial

---
+++ Pyxie.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import glob
+import Image
+
+def main(args=None):
+    listaImgCarpeta = glob.glob('input/*.png') # TODO: Cualquier formato de imagen...
+    imagenes = []
+    anchoTotal = 0
+    alturaMaxima = 0
+    
+    # Se abre cada imagen de la carpeta (que haya pasado el filtro) y se copia
+    # a una lista.
+    for imgPath in listaImgCarpeta:
+	    img = Image.open(imgPath)
+	    imagenes.append(img.copy())
+	    
+	    # Se usara para determinar el ancho total del sprite final
+	    anchoTotal += img.size[0]
+	    
+	    # Las imagenes se iran agrupando horizontalmente. Se determina la altura
+	    # maxima para usar en el sprite final.
+	    # TODO: Aprovechar al maximo el espacio.
+	    alturaMaxima = alturaMaxima > img.size[1] if alturaMaxima else img.size[1]
+	    
+    Pyxie = Image.new('RGBA', (anchoTotal, alturaMaxima), (255,255,255,0))
+
+    offsetX = offsetY = 0
+
+    for img in imagenes:
+        Pyxie.paste(img, (offsetX,0)) # Pegar cada imagen en el sprite.
+        offsetX += img.size[0] # Moverse al final de la imagen.
+    Pyxie.save('Pyxie.png', 'PNG')
+    
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,4 @@
+pyxie
+=====
+
+A small script that generates a single sprite image from a directory with input image files.
