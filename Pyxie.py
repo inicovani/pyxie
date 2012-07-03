2b3ef445d1436e2aae402b97b2700790fa76d67a
Ignacio Nicovani <inicovani@HQ>
2012-07-02 23:13:56 -0400

    Bugfix

--- Pyxie.py
+++ Pyxie.py
@@ -12,16 +12,17 @@ def main(args=None):
     # Se abre cada imagen de la carpeta (que haya pasado el filtro) y se copia
     # a una lista.
     for imgPath in listaImgCarpeta:
-	    img = Image.open(imgPath)
-	    imagenes.append([img.copy(), imgPath])
-	    
-	    # Se usara para determinar el ancho total del sprite final
-	    anchoTotal += img.size[0]
-	    
-	    # Las imagenes se iran agrupando horizontalmente. Se determina la altura
-	    # maxima para usar en el sprite final.
-	    # TODO: Aprovechar al maximo el espacio.
-	    alturaMaxima = alturaMaxima > img.size[1] if alturaMaxima else img.size[1]
+        img = Image.open(imgPath)
+        imagenes.append([img.copy(), imgPath])
+
+        # Se usara para determinar el ancho total del sprite final
+        anchoTotal += img.size[0]
+
+        # Las imagenes se iran agrupando horizontalmente. Se determina la altura
+        # maxima para usar en el sprite final.
+        # TODO: Aprovechar al maximo el espacio.
+        if img.size[1] > alturaMaxima:
+            alturaMaxima = img.size[1]
 	    
     Pyxie = Image.new('RGBA', (anchoTotal, alturaMaxima), (255,255,255,0))
 
@@ -29,11 +30,12 @@ def main(args=None):
     agregados = 0 # cantidad de archivos agregados al sprite final
 
     with open('Pyxie.css', 'w') as css:
-        css.write('.Pyxie { background: #fff url(\'Pyxie.png\') no-repeat; }\n\n')
+        css.write('.Pyxie { background: #fff url(\'Pyxie.png\') no-repeat;}\n\n')
         for img, path in imagenes:
+            ancho,alto = img.size
             Pyxie.paste(img, (offsetX,0)) # Pegar cada imagen en el sprite.
             print '[{0} agregado] w:{1} h:{2} x:{3} y:{4};'.format(path, img.size[0], img.size[1], offsetX, offsetY)
-            css.write('.Pyxie_{0} {{ background-position: -{1}px 0px !important; }}\n'.format(path[6:-4], offsetX))
+            css.write('.Pyxie_{0} {{ background-position: -{1}px 0px !important; padding-left: {2}px; }}\n'.format(path[6:-4], offsetX,ancho))
             offsetX += img.size[0] # Moverse al final de la imagen.
             agregados += 1
         Pyxie.save('Pyxie.png', 'PNG')
